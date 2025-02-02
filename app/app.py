ccfba3efa62f0da444f182c995a5add026726dbc
Harry Tripp <115900915+harrytripp@users.noreply.github.com>
2025-02-02 00:50:33 +0000

    Create database table (untested)

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 import sqlite3
-from flask import Flask, g
+from datetime import datetime
+import click
+from flask import Flask, g, current_app
 app = Flask(__name__)
 
 DATABASE = './database.db'
@@ -9,6 +11,21 @@ def get_db():
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
+
+def init_db():
+     db = get_db()
+     with current_app.open_resource('schema.sql') as file:
+          db.executescript(file.read().decode('utf8'))
+
+@click.command('init-db')
+def init_db_command():
+     """clear the existing data and create new tables"""
+     init_db()
+     click.echo('Initialised the database.')
+
+sqlite3.register_converter(
+     "timestamp", lambda v: datetime.fromisoformat(v.decode())
+)
 
 @app.teardown_appcontext
 def close_connection(exception):
